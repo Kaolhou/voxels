baf08c7a51bf3caa6a9fbd5afc6396e7830f66a6
André Mendes <andre.mendes.rocha65@gmail.com>
2023-11-17 22:38:50 -0300

    almost done v2

--- src/utils/requestFullscreen.ts
+++ src/utils/requestFullscreen.ts
@@ -1,11 +1,5 @@
 export default async function openFullscreen(elem: HTMLElement) {
   if (elem.requestFullscreen) {
     elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) {
-    /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) {
-    /* IE11 */
-    elem.msRequestFullscreen();
   }
 }
